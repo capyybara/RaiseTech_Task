77c31cca3dbde966586913cdf566aa2f1e796f03
capybara <sakibara.ex.9175@gmail.com>
2023-07-01 15:06:48 +0900

    3回目課題

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,26 @@
 //　任意のプログラムで例外を起こしてみる
 // →課題のソースコードをGitHubにPushしてレビューを依頼する。
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<String> cats = List.of("Tama", "Hachi", "Goro", "Nana");
+        List<String> dogs = new ArrayList<>();
+        dogs.add("Pochi");
+        dogs.add("Shiro");
+        dogs.add("Kuro");
 
+        // catsの名前を出力
+        for (String cat : cats) {
+            System.out.println(cat);
+        }
 
+        // dogの名前を出力
+        for (String dog : dogs) {
+            System.out.println(dog);
+        }
 
 
     }
@@ -22,3 +38,4 @@ public class Main {
 
 
 }
+
