635c35f8f4f2a3f7885630ba8f195d0fb64525c7
capybara <sakibara.ex.9175@gmail.com>
2023-07-01 14:56:23 +0900

    3回目課題

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,24 @@
 // Shift を 2 回押して 'どこでも検索' ダイアログを開き、`show whitespaces` と入力して
 // Enter キーを押します。これでコードに空白文字が表示されます。
+
+// 第3回目課題：String型のListかMapを作成し、コンソール画面に表示してみる　
+// （List内には複数入れること）
+//　任意のプログラムで例外を起こしてみる
+// →課題のソースコードをGitHubにPushしてレビューを依頼する。
+// 第3回目課題：String型のListかMapを作成し、コンソール画面に表示してみる　
+// （List内には複数入れること）
+//　任意のプログラムで例外を起こしてみる
+// →課題のソースコードをGitHubにPushしてレビューを依頼する。
+
 public class Main {
     public static void main(String[] args) {
-        // ハイライトされたテキストにキャレットがある状態で Opt+Enter を押して
-        // IntelliJ IDEA が提案する修正方法を表示します。
-        System.out.printf("Hello and welcome!");
 
-        // コードを実行するには Ctrl+R を押すか、ガターにある緑の矢印ボタンをクリックします。
-        for (int i = 1; i <= 5; i++) {
 
-            // Ctrl+D を押してコードのデバッグを開始します。ブレークポイントを 1 つ設定しましたが、
-            // Cmd+F8 を押すといつでも他のブレークポイントを追加できます。
-            System.out.println("i = " + i);
-        }
+
+
     }
+
+
+
+
 }
